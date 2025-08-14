f5d25d578668785bd86f760024e907e53663cc1c
Kalleboo84 <andreas_karlberg@hotmail.com>
2025-08-14 21:11:58 +0200

    Create question.dart

---
+++ lib/data/question.dart
@@ -0,0 +1,28 @@
+class Question {
+  final String question;
+  final List<String> choices;
+  final String answer;
+  final String imageUrl;     // Kan vara "TBD"
+  final String attribution;  // Kan vara "TBD"
+  final String century;
+
+  Question({
+    required this.question,
+    required this.choices,
+    required this.answer,
+    required this.imageUrl,
+    required this.attribution,
+    required this.century,
+  });
+
+  factory Question.fromJson(Map<String, dynamic> j) {
+    return Question(
+      question: j['question'] as String,
+      choices: (j['choices'] as List).map((e) => e.toString()).toList(),
+      answer: j['answer'] as String,
+      imageUrl: (j['imageUrl'] ?? '').toString(),
+      attribution: (j['attribution'] ?? '').toString(),
+      century: (j['century'] ?? '').toString(),
+    );
+  }
+}
