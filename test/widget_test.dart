2ee4f32b8fb2f0faad1f0cc3d322753dd978f1fa
Kalleboo84 <andreas_karlberg@hotmail.com>
2025-08-16 00:20:13 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-// Importera din app direkt från lib med relativ sökväg
-import '../lib/main.dart';
+// Viktigt: detta måste matcha name: i pubspec.yaml
+// (jag utgår från: name: guess_heritage)
+import 'package:guess_heritage/main.dart';
 
 void main() {
   testWidgets('Appen bygger en MaterialApp', (tester) async {
