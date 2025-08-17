9b28f2d4e515aabf5680cfae82e473283f88ff5a
Kalleboo84 <andreas_karlberg@hotmail.com>
2025-08-17 23:40:42 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,8 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:guess_the_heritage/ui/home_screen.dart';
-import 'package:guess_the_heritage/services/background_music.dart' as music;
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  // Rita UI direkt
   runApp(const MyApp());
-
-  // Initiera bakgrundsmusiken i bakgrunden (blockerar inte start)
-  unawaited(music.BackgroundMusic.instance.init());
 }
 
 class MyApp extends StatelessWidget {
