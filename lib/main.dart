34175392a96c07b790c3decdf4fc2a45e24eba52
Kalleboo84 <andreas_karlberg@hotmail.com>
2025-08-18 23:19:53 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:guess_the_heritage/ui/home_screen.dart';
+import 'package:guess_the_heritage/services/background_music.dart' as music;
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    // ✅ Starta bakgrundsmusiken direkt när appen startar (loopar vidare).
+    music.BackgroundMusic.instance.ensureStarted();
+  }
 
   @override
   Widget build(BuildContext context) {
